976ae1ce62670e2be0ca9c4b9fbafe191007558e
Alex Bonwit <alexbonwit@mac.com>
2019-11-14 23:49:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,46 @@
 # My Code here....
+
+def map_to_negative(source)
+  neg_array = []
+  counter = 0
+  
+  while counter < source.length do
+    source[counter] *= -1
+    neg_array << source[counter]
+    counter += 1
+  end
+  neg_array
+end
+
+def map_to_no_change(source)
+  
+  
+end
+
+def map_to_double(source)
+  
+  
+end
+
+def map_to_square(source)
+  
+  
+end  
+
+
+
+def reduce_to_total(source, start)
+
+
+end
+
+def reduce_to_all_true(source)
+
+
+end
+
+def reduce_to_any_true(source)
+  
+  
+end  
+
